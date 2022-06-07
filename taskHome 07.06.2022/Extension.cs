a581fec2c72598b50c5e6bb52f30c7692bb32c5e
User <tamerlankha@code.edu.az>
2022-06-07 18:57:18 +0400

    Add project files.

---
+++ taskHome 07.06.2022/Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskHome_07._06._2022
+{
+    static class Extensions
+    {
+        public static void Add(this int[] arr, int a) 
+        {
+            
+            Array.Resize(ref arr, arr.Length+1);
+            arr[arr.Length - 1] = a;
+            
+        }
+    }
+}
